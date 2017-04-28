519ff68d7eef613cb129342a73b994a68d859f94
gidhanbagus <gidhanbagus7@gmail.com>
2017-04-28 17:57:55 +0700

    Update debian732kvm.sh

--- debian732kvm.sh
+++ debian732kvm.sh
@@ -121,7 +121,7 @@ service ssh restart
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 80 -p 109 -p 110"/g' /etc/default/dropbear
+echo "OPTIONS=\"-p 80 -p 109 -p 110 -b /etc/pesan\"" > /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 service ssh restart
 service dropbear restart
